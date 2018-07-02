d32cf1e21833f5ef25df38205e1f29873f0491c1
Isaac Carol Weisberg <isaac_weisberg@outlook.com>
2018-07-02 19:52:52 +0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Lize",
+    products: [
+        .library(
+            name: "Lize",
+            targets: ["Lize"]),
+    ],
+    dependencies: [
+    
+    ],
+    targets: [
+        .target(
+            name: "Lize",
+            dependencies: [ ])
+    ]
+)
